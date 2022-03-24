3d2e918bbfa27ed0b36dafca2f231256511cd577
PIAI21JUL3006 <tanushreekohad01@gmail.com>
2022-03-24 12:38:12 +0530

    Updated the remaining SQL queries

--- Joins HANDS-ON (21 March).sql
+++ Joins HANDS-ON (21 March).sql
@@ -64,7 +64,7 @@ select order_no, order_date, purch_amt from Orders where salesman_id = 5001
 select customer_id, cust_name,city, grade, salesman_id from Customer where city='New York' and grade>100
 
 --6.	write a SQL query to find the details of those salespeople whose commissions range from 0.10 to 0.12. Return salesman_id, name, city, and commission
-select * from Salesman where commision>=0.10 AND commision<=0.12
+select * from Salesman where (commision>=0.10 AND commision<=0.12)
 
 --7.	write a SQL query to calculate total purchase amount of all orders. Return total purchase amount.
 select sum(purch_amt) from Orders
@@ -73,27 +73,39 @@ select sum(purch_amt) from Orders
 select AVG(purch_amt) from Orders
 
 --9.	write a SQL query to count the number of unique salespeople. Return number of salespeople.
-select distinct count(name) from Salesman
+select count(distinct Salesman_id) from Orders
 
 --10.	write a SQL query to find the highest purchase amount ordered by each customer. Return customer ID, maximum purchase amount
 select customer_id, MAX(purch_amt) from Orders group by customer_id
 
 --11.	write a SQL query to find the highest purchase amount ordered by each customer on a particular date. Return, order date and highest purchase amoun
---select order_date, MAX(purch_amt) from Orders where order_date='2012-09-10' group by customer_id
+select customer_id order_date, MAX(purch_amt) from Orders group by customer_id, order_date
 
 --12.	write a SQL query to find the highest purchase amount on '2012-08-17' by each salesperson. Return salesperson ID, purchase amount. 
---select salesman_id, MAX(purch_amt) from Orders group by salesman_id having order_date='2012-08-17'
+select salesman_id, MAX(purch_amt) from Orders group by salesman_id,order_date having order_date='2012-08-17'
+
+--OR
+SELECT salesman_id,MAX(purch_amt) FROM orders 
+WHERE order_date = '2012-08-17' GROUP BY salesman_id;
 
 --13.	write a SQL query to find the salesperson and customer who belongs to same city. Return Salesman, cust_name and city.
-select Salesman.Salesman_id, cust_name, Customer.city from Salesman full outer join Customer on Salesman.city = Customer.city
+select Salesman.name as "Salesman_name", Customer.cust_name, Customer.city from Salesman, Customer where salesman.city=customer.city;
 
 --14.	write a SQL query to find those orders where order amount exists between 500 and 2000. Return ord_no, purch_amt, cust_name, city
 select order_no, purch_amt, cust_name, city from Orders full outer join Customer on Customer.customer_id = Orders.customer_id
 where (purch_amt> 500 AND purch_amt < 2000)
 
+--OR
+select  a.order_no, a.purch_amt, b.cust_name, b.city from Orders a,Customer b 
+where a.customer_id = b.customer_id 
+AND a.purch_amt BETWEEN 500 AND 2000;
+
 --15.	write a SQL query to find those salespersons who received a commission from the company more than 12%. Return Customer Name, customer city, Salesman, commission
-select Salesman_id, name, city, commision from Salesman where commision>0.12
+select a.cust_name AS "Customer Name", a.city, b.name AS "Salesman", b.commision 
+from customer a INNER JOIN salesman b 
+on a.salesman_id=b.salesman_id where b.commision>.12;
 
 --16.	write a SQL query to display the cust_name, customer city, grade, Salesman, salesman city. The result should be ordered by ascending on customer_id.
-select cust_name, Customer.city, grade, Customer.Salesman_id, Salesman.city from Customer full outer join Salesman 
-on Salesman.name = Customer.cust_name order by customer_id
+select a.cust_name, a.city, a.grade, b.name AS "Salesman_name",b.city 
+from customer a LEFT JOIN Salesman b 
+ON a.salesman_id = b.salesman_id  order by a.customer_id;
